d12eb25a8206b983f2a5e1c3013629951f3e77ba
viet <vietvodoi1232@gmail.com>
2023-10-30 10:26:54 +0700

    product3

--- backend-ecomerce/controllers/productControllers.js
+++ backend-ecomerce/controllers/productControllers.js
@@ -3,38 +3,46 @@ const Product = require("../models/Product");
 // Lấy tất cả sản phẩm
 const getAllProducts = async (req, res) => {
   try {
-    let { page, pageSize, name, category } = req.query;
-
-    // Xác định giá trị mặc định nếu các tham số không tồn tại
-    if (!page) page = 1;
-    if (!pageSize) pageSize = 10;
-
-    const filter = {};
-    if (name) {
-      filter.name = { $regex: name, $options: "i" };
-    }
-    if (category) {
-      filter.category = { $regex: category, $options: "i" };
-    }
-
-    const totalRecords = await Product.countDocuments(filter);
-    const totalPages = Math.ceil(totalRecords / pageSize);
-
-    const products = await Product.find(filter)
-      .skip((page - 1) * pageSize)
-      .limit(parseInt(pageSize));
-
-    res.status(200).json({
-      data: products,
-      page: parseInt(page),
-      pageSize: parseInt(pageSize),
-      totalPages,
-      totalRecords,
-    });
+    const products = await Product.find({});
+    res.json(products);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: "Không thể lấy danh sách sản phẩm" });
   }
 };
+// const getAllProducts = async (req, res) => {
+//   try {
+//     let { page, pageSize, name, category } = req.query;
+
+//     // Xác định giá trị mặc định nếu các tham số không tồn tại
+//     if (!page) page = 1;
+//     if (!pageSize) pageSize = 10;
+
+//     const filter = {};
+//     if (name) {
+//       filter.name = { $regex: name, $options: "i" };
+//     }
+//     if (category) {
+//       filter.category = { $regex: category, $options: "i" };
+//     }
+
+//     const totalRecords = await Product.countDocuments(filter);
+//     const totalPages = Math.ceil(totalRecords / pageSize);
+
+//     const products = await Product.find(filter)
+//       .skip((page - 1) * pageSize)
+//       .limit(parseInt(pageSize));
+
+//     res.status(200).json({
+//       data: products,
+//       page: parseInt(page),
+//       pageSize: parseInt(pageSize),
+//       totalPages,
+//       totalRecords,
+//     });
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// };
 
 // Lấy sản phẩm theo ID
 const getProductById = async (req, res) => {
