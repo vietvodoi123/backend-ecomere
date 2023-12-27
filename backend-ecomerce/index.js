76056d51e528199d8de8971944a0f496969b1e14
viet <vietvodoi1232@gmail.com>
2023-12-27 22:49:13 +0700

    commit 5

--- backend-ecomerce/index.js
+++ backend-ecomerce/index.js
@@ -9,9 +9,15 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Cho phép tất cả các nguồn gốc
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
