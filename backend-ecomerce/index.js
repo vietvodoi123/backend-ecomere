ff3c76081dc8a494bc12941e6d8020090b280839
viet <vietvodoi1232@gmail.com>
2023-12-27 22:53:58 +0700

    commit 6

--- backend-ecomerce/index.js
+++ backend-ecomerce/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
