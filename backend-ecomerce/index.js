795fc80fda0686b533fa04a136d1c7f07cddbc9e
viet <vietvodoi1232@gmail.com>
2023-12-27 22:29:54 +0700

    commit 3

--- backend-ecomerce/index.js
+++ backend-ecomerce/index.js
@@ -8,12 +8,7 @@ const { connectDB } = require("./config/db");
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Cho phép tất cả các nguồn gốc
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors());
 
 connectDB();
 app.get("/", (req, res) => {
