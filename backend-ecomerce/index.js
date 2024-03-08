01e34230bf3a0ffb9738b9b61b8e6c576be3b32e
viet <vietvodoi1232@gmail.com>
2024-03-08 23:06:28 +0700

    commit finish

--- backend-ecomerce/index.js
+++ backend-ecomerce/index.js
@@ -11,10 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000" | "https://femyapp.onrender.com"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://femyapp.onrender.com");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
