a9fa4d794b4eb57c0aa2f47ff02ac7c8ab2cb746
viet <vietvodoi1232@gmail.com>
2024-03-16 21:52:23 +0700

    add cv

--- backend-ecomerce/index.js
+++ backend-ecomerce/index.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://femyapp.onrender.com");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://myshop213.netlify.app/"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
